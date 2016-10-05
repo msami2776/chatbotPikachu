e8c17a57428d42bc74f24ef936010710257dcaee
Student 6 <Student 6@10.8.33.165>
2016-10-05 13:15:20 -0400

    complete jokenorepeat at home

--- ChatbotPikachu/src/groupFiles/ChuJoke.java
+++ ChatbotPikachu/src/groupFiles/ChuJoke.java
@@ -6,13 +6,17 @@ public class ChuJoke implements Topic {
 
 	private boolean inJokeLoop;
 	private String jokeResponse;
-	private String jokes[] = {"joke1", "joke2", "joke3"};
-	private int[] usedJokes = new int[10];
-	private int jokeNum = 10;
+	private String jokes[] = {"joke1", "joke2", "joke3"};//add jokes later
+	private String usedJokes[] = {"", "", ""}; //add used jokes to here. once array is full, have computer look for full array and say "no more jokes"
+	private int jokeNum = 10; //only 10 jokes
 
 	public void talk() {
 		int jokeIndex = randInt(0, jokes.length);
 		String joke = jokes[jokeIndex];
+		if(usedJokes[9] == jokes[0]){
+			System.out.println("I don't want to tell you any more jokes");
+			MakinoonName.talkForever();
+		}
 		inJokeLoop = true;
 		while (inJokeLoop){
 			MakinoonName.print("Oh, you wanna hear a joke?");
