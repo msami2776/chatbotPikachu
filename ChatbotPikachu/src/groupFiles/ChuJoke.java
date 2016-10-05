178f2879f17e00e1e7c11217b3d474a4aa4744a6
Student 6 <Student 6@10.8.33.165>
2016-10-05 13:05:03 -0400

    trying not to repeat jokes

--- ChatbotPikachu/src/groupFiles/ChuJoke.java
+++ ChatbotPikachu/src/groupFiles/ChuJoke.java
@@ -1,30 +1,47 @@
 package groupFiles;
 
+import java.util.Random;
+
 public class ChuJoke implements Topic {
 
 	private boolean inJokeLoop;
 	private String jokeResponse;
+	private String jokes[] = {"joke1", "joke2", "joke3"};
+	private int[] usedJokes = new int[10];
+	private int jokeNum = 10;
 
 	public void talk() {
+		int jokeIndex = randInt(0, jokes.length);
+		String joke = jokes[jokeIndex];
 		inJokeLoop = true;
 		while (inJokeLoop){
-			MakinoonName.print("How about a joke?");
+			MakinoonName.print("Oh, you wanna hear a joke?");
 			jokeResponse = MakinoonName.getInput();
 			if(jokeResponse.indexOf("stop") >= 0){
 				inJokeLoop = false;
 				MakinoonName.talkForever();
 			}
 			else{
-				MakinoonName.print("");
+				MakinoonName.print("I'll take that as a yes.");
 			}
 		}
 	}
+	
+	public static int randInt(int min, int max) {
+		Random gen = new Random();
+	    int randomNum = gen.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
 
 	public boolean isTriggered(String userInput) {
-		if(MakinoonName.findKeyword(userInput, "", 0) >= 0){
+		if(MakinoonName.findKeyword(userInput, "joke", 0) >= 0){
 			return true;
 		}
-		if(MakinoonName.findKeyword(userInput, "", 0) >= 0){
+		if(MakinoonName.findKeyword(userInput, "bored", 0) >= 0){
+			return true;
+		}
+		if(MakinoonName.findKeyword(userInput, "boring", 0) >= 0){
 			return true;
 		}
 		return false;
