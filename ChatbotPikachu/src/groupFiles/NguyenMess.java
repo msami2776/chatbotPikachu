f96392ba64bdeb24834dbdeee6ee6753fea629f2
Student 6 <Student 6@10.8.33.171>
2016-10-06 12:15:54 -0400

    Final Adjustments 10/6/16

--- ChatbotPikachu/src/groupFiles/NguyenMess.java
+++ ChatbotPikachu/src/groupFiles/NguyenMess.java
@@ -1,35 +1,4 @@
-/*
-package groupFiles;
-public class NguyenMess implements Topic {
-	
-	private boolean inMessLoop;
-	private String messResponse;
-	public void talk() {
-		inMessLoop = true;
-		while (inMessLoop){
-			MakinoonName.print("Tell me about school.");
-			messResponse = MakinoonName.getInput();
-			if(messResponse.indexOf("stop") >= 0){
-				inMessLoop = false;
-				MakinoonName.talkForever();
-			}
-			else{
-				MakinoonName.print("That's my favorite " + "part about school too.");
-			}
-		}
-	}
-	public boolean isTriggered(String userInput) {
-		if(MakinoonName.findKeyword(userInput, "homework", 0) >= 0){
-			return true;
-		}
-		if(MakinoonName.findKeyword(userInput, "class", 0) >= 0){
-			return true;
-		}
-		return false;
-	}
-}
-*/
-//gives the Chatbot an atitude
+//Chatbot replying to rude comments
 package groupFiles;
 import java.util.Random;
 public class NguyenMess implements Topic {
@@ -37,66 +6,76 @@ public class NguyenMess implements Topic {
 	//private boolean inMessLoop;
 	//private String messResponse;
 	private int shutUpCount = 0;
-	private String repeatedResponse;
 	private String whichResponse;//checks the response to see how to reply
-	
-	
 	private String messResponse;
-	private int responseLength;
-	private int lastCharacter;
+	private boolean inIgnoreLoop = false; //check if chatbot will ignore you
+	private int emptyResponseTolerance = 0;//
+	private int silentCounter = 0;
 	
-	String[] shutUpResponseArray = {"HEY! You can't tell me what to do.", "Or what?", "Don't boss me!","Why so rude?!"}; 
+	String[] shutUpResponseArray = {"HEY! You can't tell me what to do. Or I'll haunt you forever!", "Or what?", "Don't boss me!","Why so rude?!"}; 
 	String[] questionResponseArray = {"Do I have to answer that question?", "Can I not answer that question?", "Do I HAVE to answer?"}; 
-	 
+	String[] emptyResponseArray = {"Can you please type something?","Type something PLEASE.","Guess we're not talking."};
 	
 
 	public void talk() {
-		if(whichResponse == "1")
-		{
-			shutUpCount++;
-			MakinoonName.print(shutUpResponseArray[randomNumberRange(4)]);
+		if(whichResponse =="5"){
+			MakinoonName.print("I am talking oh wait I'm a ghost. I'm surprised you know my presence!");
 		}
-		if(whichResponse == "2")
-		{
-			if(shutUpCount <= 0)
-			{
-				MakinoonName.print("I might be programmed to be a ghost, but I'm actually a program.  No need to say sorry.");
+			if(whichResponse == "1"){
+				shutUpCount++;
+				MakinoonName.print(shutUpResponseArray[randomNumberRange(4)]);
 			}
-			if(shutUpCount == 1)
-			{
-				MakinoonName.print("It's ok.");
+			if(whichResponse == "2"){
+				if(shutUpCount <= 0){
+					MakinoonName.print("I might be programmed to be a ghost, but I'm actually a program.  No need to say sorry.");
+				}
+				if(shutUpCount == 1){
+					MakinoonName.print("It's ok.");
+				}
+				if(shutUpCount > 1){
+					MakinoonName.print("Are you sure about that? You just told be to shut up "+shutUpCount+" times!");
+				}
 			}
-			if(shutUpCount > 1)
-			{
-				MakinoonName.print("Are you sure about that? You just told be to shut up "+shutUpCount+" times!");
-			}			
-		}
-		if(whichResponse == "3")
+			if(whichResponse == "3"){
+				MakinoonName.print(questionResponseArray[randomNumberRange(3)]);
+			}
+			if(whichResponse =="4"){
+				emptyResponseTolerance++;
+				MakinoonName.print(emptyResponseArray[emptyResponseTolerance-1]);
+				if(emptyResponseTolerance>=3){
+					inIgnoreLoop=true;
+					ignore();
+				}	
+			}
+			MakinoonName.talkForever();
+	}		
+		
+		
+		
+		
+		
+	private void ignore() {
+		while(inIgnoreLoop)
 		{
-			MakinoonName.print(questionResponseArray[randomNumberRange(3)]);
+			//System.out.println("TEST");
+			messResponse = MakinoonName.getInput();
+			if(MakinoonName.findKeyword(messResponse, "please talk", 0) >= 0){
+				inIgnoreLoop=false;
+				emptyResponseTolerance = 0;
+				MakinoonName.print("OK...I'll talk.");
+				MakinoonName.talkForever();
+			}
+			else{
+				silentCounter++;
+				if(silentCounter>3){
+					MakinoonName.print("if you want me to respond, type in 'please talk'.");
+				}
+				else{
+					MakinoonName.print("...");
+				}
+			}
 		}
-		//System.out.println(messResponse);
-		//System.out.println(responseLength);
-		//System.out.println(lastCharacter);
-		
-		MakinoonName.talkForever();
-		
-		
-		
 			
-//			inMessLoop = true;
-//			while (inMessLoop){
-//				MakinoonName.print("I don't really feel like answering.");
-//				messResponse = MakinoonName.getInput();
-//				if(messResponse.indexOf("stop") >= 0){
-//					inMessLoop = false;
-//					MakinoonName.talkForever();
-//				}
-//				else{
-//					MakinoonName.print("That's my favorite " + "part about school too.");
-//				}
-//			}
-		
 	}
 	public static int randomNumberRange(int range){
 		Random gen = new Random();
@@ -107,18 +86,28 @@ public class NguyenMess implements Topic {
 	}
 	public boolean isTriggered(String userInput) {
 		messResponse = userInput;
-		responseLength = userInput.length();
-		if(MakinoonName.findKeyword(userInput, "shut up", 0) >= 0 || MakinoonName.findKeyword(userInput, "be quiet", 0) >= 0){
-			whichResponse = "1";
-			return true;
-		}
-		else if(messResponse.indexOf("?")>-1){
+		//responseLength = userInput.length();
+		if(messResponse.indexOf("?")>-1){
 			whichResponse = "3";
 			return true;
 		}
-		if(MakinoonName.findKeyword(userInput, "sorry", 0) >= 0 || MakinoonName.findKeyword(userInput, "i didn't mean it", 0) >= 0){
-			whichResponse = "2";
-			return true;
+		else{
+			if(MakinoonName.findKeyword(userInput, "shut up", 0) >= 0 || MakinoonName.findKeyword(userInput, "be quiet", 0) >= 0){
+				whichResponse = "1";
+				return true;
+			}
+			if(MakinoonName.findKeyword(userInput, "sorry", 0) >= 0 || MakinoonName.findKeyword(userInput, "i didn't mean it", 0) >= 0){
+				whichResponse = "2";
+				return true;
+			}
+			if(MakinoonName.findKeyword(userInput, "please talk", 0) >= 0){
+				whichResponse = "5";
+				return true;
+			}
+			if(MakinoonName.findKeyword(userInput, "", 0) >= 0){
+				whichResponse = "4";
+				return true;
+			}
 		}
 		return false;
 	}
