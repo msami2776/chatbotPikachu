f92d4da853411ba5dc98222a664cfbed72196981
NikitaSmirnoff <nsmirnov0027@bths.edu>
2016-10-07 03:24:44 -0400

    Complete

--- ChatbotPikachu/src/groupFiles/NikitaMath.java
+++ ChatbotPikachu/src/groupFiles/NikitaMath.java
@@ -4,41 +4,126 @@ public class NikitaMath implements Topic{
 	
 	private boolean inMathLoop;
 	private String mathResponse;
-	private String mathResponse2;
 	private String operation;
-	private String operationPsn;
+	private int operationPsn;
 
 	public void talk() {
-		System.out.println("TALK STARTED");
-		String[] operationsArray = {"+", "-", "*", "/", "<", ">", "%"};
+		String[] operationsArray = {"+", "-", "*", "/", "<", ">", "="};
+		String[] alphabetArray = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 		
 		inMathLoop = true;
 		
 		while (inMathLoop){
-			mathResponse = MakinoonName.getInput();
+			mathResponse = MakinoonName.getInput().replaceAll("\\s", "");
+			// replaceAll replaces all space characters with nothing.
 			
-			if(MakinoonName.findKeyword(mathResponse, "yes", 0) >= 0){
-				System.out.println("Okay! Throw an equation at me.");
-				mathResponse2 = MakinoonName.getInput();
-				for(int i = 0; i < operationsArray.length; i++){
-					if(MakinoonName.findKeyword(mathResponse2, operationsArray[i], 0) >= 0){
-						int operationPsn = MakinoonName.findKeyword(mathResponse2, "yes", 0);
-					}
+			for(int i = 0; i < operationsArray.length; i++){
+				if(MakinoonName.findKeyword(mathResponse, operationsArray[i], 0) >= 0){
+					operation = operationsArray[i];
+					operationPsn = MakinoonName.findKeyword(mathResponse, operation, 0);
 				}
 			}
 			
-			if(operationPsn >= 0){
-				String valueOne = mathResponse.substring(operationPsn - 2);
-				System.out.println("The first value is: " + valueOne);
-				String valueTwo = mathResponse.substring(operationPsn + 2);
-				System.out.println("The second value is: " + valueTwo);
-				String result = valueOne + " " + operation + " " + valueTwo;
-				MakinoonName.print(result);
+			if(operationPsn >= 1){
+				
+				for(int i = 0; i < alphabetArray.length; i++){
+					if(MakinoonName.findKeyword(mathResponse, alphabetArray[i], 0) >= 0){
+						inMathLoop = false;
+						MakinoonName.talkForever();
+					}
+				}
+				
+				int valueOne = mathResponse.charAt(operationPsn - 1) - 48;
+				int valueTwo = mathResponse.charAt(operationPsn + 1) - 48;
+				
+				if(operation == "+"){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					int result = valueOne + valueTwo;
+					System.out.println("The answer is: " + result);
+				}else{
+					if(operation == "-"){
+						System.out.println("The first value is: " + valueOne);
+						System.out.println("The second value is: " + valueTwo);
+						int result = valueOne - valueTwo;
+						System.out.println("The answer is: " + result);
+					}else{
+						if(operation == "*"){
+							System.out.println("The first value is: " + valueOne);
+							System.out.println("The second value is: " + valueTwo);
+							int result = valueOne * valueTwo;
+							System.out.println("The answer is: " + result);
+						}else{
+							if(operation == "/"){
+								System.out.println("The first value is: " + valueOne);
+								System.out.println("The second value is: " + valueTwo);
+								int result = valueOne / valueTwo;
+								System.out.println("The answer is: " + result);
+							}
+						}
+					}
+				}
+				
+				if(operation == "=" && valueOne == valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: true");
+				}
+				if(operation == "=" && valueOne != valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: false");
+				}
+				
+				if(operation == "<" && valueOne < valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: true");
+				}
+				
+				if(operation == "<" && valueOne > valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: false");
+				}
+				
+				if(operation == "<" && valueOne == valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: false");
+				}
+				
+				if(operation == ">" && valueOne > valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: true");
+				}
+				
+				if(operation == ">" && valueOne < valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: false");
+				}
+				
+				if(operation == ">" && valueOne == valueTwo){
+					System.out.println("The first value is: " + valueOne);
+					System.out.println("The second value is: " + valueTwo);
+					System.out.println("The answer is: false");
+				}
 				
 			}else{
-				MakinoonName.print("I don't understand you");
+				MakinoonName.print("That's not a math equation! Try again or stop?");
 			}
 			
+			if(mathResponse.indexOf("stop") >= 0){
+				inMathLoop = false;
+				MakinoonName.talkForever();
+			}
+			
+			if(mathResponse.indexOf("nevermind") >= 0){
+				inMathLoop = false;
+				MakinoonName.talkForever();
+			}
 		}
 	}
 
@@ -48,7 +133,7 @@ public class NikitaMath implements Topic{
 		
 		for(int i = 0; i < triggerArray.length; i++){
 			if(MakinoonName.findKeyword(userInput, triggerArray[i], 0) >= 0){
-				System.out.println("Do you want me to solve something?");
+				System.out.println("I can solve something with whole numbers for you! Try me!");
 				return true;
 			}
 		}
